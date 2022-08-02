0fe59c175796baa22a01b4c9d17e59bc14dfd3f0
Welll192 <maxwell.uni19.2@gmail.com>
2022-08-02 12:41:30 -0500

    implementando la clase BrindarServicio donde va la lógica aplicada al objeto de acuerdo a su referencia de clase

---
+++ BrindarServicio.java
@@ -0,0 +1,23 @@
+public class BrindarServicio implements Visitor {
+
+    @Override
+    public void visitEconomic(HabitacionEconomica he) {
+        System.out.println("este es un servicio economico");
+    }
+
+    @Override
+    public void visitNormal(HabitacionNormal hn) {
+        System.out.println("este es un servicio normal");
+    }
+
+    @Override
+    public void visitPremium(HabitacionPremium hp) {
+        System.out.println("este es un servicio premium");
+    }
+
+    @Override
+    public void visitFamiliar(HabitacionFamiliar hf) {
+        System.out.println("este es un servicio familiar");
+    }
+
+}
