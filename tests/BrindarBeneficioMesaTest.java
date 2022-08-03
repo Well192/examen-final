6253a1a5b1c7a66906700257054eb5bdd2734213
Welll192 <maxwell.uni19.2@gmail.com>
2022-08-03 13:52:08 -0500

    test para la clase BeneficioRestaurante

--- tests/BrindarBeneficioMesaTest.java
+++ tests/BrindarBeneficioMesaTest.java
@@ -1,16 +1,26 @@
 package tests;
 
+import ServicioRestaurante.BrindarBeneficioMesa;
+import ServicioRestaurante.MesaSimple;
+import ServicioRestaurante.MesaVIP;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BrindarBeneficioMesaTest {
 
+    BrindarBeneficioMesa bbm = new BrindarBeneficioMesa();
     @Test
     void visitMesaSimple() {
+        MesaSimple ms = new MesaSimple("simple",50.0f);
+        System.out.println(ms.accept(bbm));
+        assertEquals("Entrada gratis",ms.accept(bbm));
     }
 
     @Test
     void visitMesaVIP() {
+        MesaVIP mvip = new MesaVIP("vip",550.5f);
+        System.out.println(mvip.accept(bbm));
+        assertEquals("Postres gratis con entrada",mvip.accept(bbm));
     }
 }
